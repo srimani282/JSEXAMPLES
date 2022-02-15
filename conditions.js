a0a96626e7c582fc962475f75c55fb18d102766f
srimani282 <srimanigoud@gmail.com>
2022-02-15 21:55:58 +0530

    Functions abd Conditions js

---
+++ conditions.js
@@ -0,0 +1,27 @@
+var x = "10";
+var y = 110;
+
+// if (x == y) {
+//   console.log("tyrue valyuhjsvcjhas");
+// } else if (true) {
+//   console.log("false got exasdahf");
+// } else if (false) {
+//   console.log("2nd else if");
+// } else {
+//   console.log("else block");
+// }
+
+console.log("all done");
+
+//x == y ? console.log("tyrue valyuhjsvcjhas") : console.log("else block");
+
+// x == y
+//   ? console.log("tyrue valyuhjsvcjhas")
+//   : false
+//   ? console.log("2nd place")
+//   : console.log("else block");
+
+//== === []
+
+// console.log(x == y);
+// console.log(x === y);
---
+++ function.js
@@ -0,0 +1,11 @@
+function numbers(data, data2) {
+  console.log(data, data2);
+  return data;
+}
+
+//var x = numbers(50000, 50000000000);
+//console.log(x);
+
+const fn = (data, data2) => console.log(data, data2);
+
+fn(500, 200);
