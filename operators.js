c8a4ce1605533c0685b5f6d3c3cfd92f96f7cab2
srimani282 <srimanigoud@gmail.com>
2022-02-15 21:57:07 +0530

    Operators.js added

---
+++ operators.js
@@ -0,0 +1,15 @@
+// // + - * / % ++ --
+// var a = 103 % 20;
+// console.log(a);
+
+// var b = "sri " + "mani";
+// console.log(b);
+
+// var c = 10 + 30 + " Sri";
+// console.log(c);
+
+var x = 20;
+x *= 30; // x = x + 30
+console.log(x);
+
+console.log(30 >= 30);
