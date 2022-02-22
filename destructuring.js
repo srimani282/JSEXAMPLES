eb020062c04796aae2d168070fcdf0c87d7eb5db
srimani282 <srimanigoud@gmail.com>
2022-02-22 21:37:28 +0530

    Destruing of obj and array

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+var obj = {
+  name: "sri",
+  cls: "degree",
+  course: "react",
+};
+
+//console.log(obj.class);
+
+const { cls, name } = obj;
+
+console.log(cls);
+
+var arr = [2, 3, 47, 9];
+
+const [num1, num2, num3] = arr;
+console.log(num3);
